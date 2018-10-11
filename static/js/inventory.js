3c29c20a69975d809acaee84452b0672b62cbd67
Brian Mwaniki <bryanmwaniqi@gmail.com>
2018-10-11 16:43:43 +0300

    [Feature #161128469] Add confirmation prompts for crud operations javascript

---
+++ static/js/inventory.js
@@ -0,0 +1,11 @@
+
+function updateProduct() {
+    confirm("Are you sure you want to update this product?");
+}
+
+function deleteProduct() {
+    confirm("Are you sure you want to delete this product?");
+}
+function createProduct() {
+    confirm("Are you sure you want to Add this product?");
+}
