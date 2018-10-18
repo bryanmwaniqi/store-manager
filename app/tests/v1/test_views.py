3d9d52bed79a1cbad140312b7ebbed87244ae44b
brian <bryanmwaniqi@gmail.com>
2018-10-18 23:43:52 +0300

    [ch #161271152] Add test method for get all products

--- app/tests/v1/test_views.py
+++ app/tests/v1/test_views.py
@@ -12,7 +12,9 @@ class TestEndpoints(unittest.TestCase):
 		url = self.app.post('/store-manager/api/v1/products', data = json.dumps(test_data), content_type = 'application/json')
 		self.assertEquals(url.status_code, 201)
 
-	
+	def test_get_all_products(self):
+		url = self.app.get('/store-manager/api/v1/products')
+		self.assertEquals(url.status_code, 200)
 
 if __name__ =='__main__':
 	app.run(exit=false)
