ea03276ce60e76d97c99aba8f7004f0336eec443
brian <bryanmwaniqi@gmail.com>
2018-10-18 10:39:36 +0300

    [finishes #161270832] Add post product test data field quantity

--- sm-app/tests/v1/test_views.py
+++ sm-app/tests/v1/test_views.py
@@ -8,7 +8,7 @@ class TestEndpoints(unittest.TestCase):
 
 
 	def test_post_product(self):
-		test_data = {'item_id':1, 'name':'iphone 7', 'category':'phones', 'price':200 }
+		test_data = {'item_id':1, 'name':'iphone 7', 'category':'phones', 'price':300, 'quantity': 4 }
 		url = self.app.post('/store-manager/api/v1/products', data = json.dumps(test_data), content_type = 'application/json')
 		self.assertEquals(url.status_code, 201)
 
