1cda78aab2729c5435774a6e8821455c9cc74f40
brian <bryanmwaniqi@gmail.com>
2018-10-18 10:33:07 +0300

    [chore #161270832] Add post product test method

---
+++ sm-app/tests/v1/test_views.py
@@ -0,0 +1,18 @@
+import unittest
+import json
+from api.v1.views import products
+
+class TestEndpoints(unittest.TestCase):
+	def setup(self):
+		self.app.test_client()
+
+
+	def test_post_product(self):
+		test_data = {'item_id':1, 'name':'iphone 7', 'category':'phones', 'price':200 }
+		url = self.app.post('/store-manager/api/v1/products', data = json.dumps(test_data), content_type = 'application/json')
+		self.assertEquals(url.status_code, 201)
+
+	
+
+if __name__ =='main':
+	app.run(exit=false)
