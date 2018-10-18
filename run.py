f11958200b0fc7fffe28f64d7a59c4a16bd64d04
brian <bryanmwaniqi@gmail.com>
2018-10-18 11:00:31 +0300

    [feature #161271003] Add api version-1 blueprint object

---
+++ run.py
@@ -0,0 +1,5 @@
+from sm-app.api.v1.views import app
+
+if __name__ == '__main__':
+	app.run()
+
---
+++ sm-app/api/v1/views.py
@@ -0,0 +1,5 @@
+from flask import blueprint
+from flask_restful import Api,Resource
+
+api_v1_blueprint = Bueprint('api-v1', __name__)
+
