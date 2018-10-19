c22583ae654c79d599f1ab11bc75e646191eb1d6
brian <bryanmwaniqi@gmail.com>
2018-10-19 03:02:01 +0300

    [ft #161271543] Add sales resource and post method for checking product availability and decrement

--- app/api/v1/views.py
+++ app/api/v1/views.py
@@ -2,6 +2,7 @@ from flask import request
 from flask_restful import Api,Resource
 
 products_list = []
+sale_orders_list = []
 
 class AllProducts(Resource):
 
@@ -39,7 +40,7 @@ class SpecificProduct(Resource):
 	def get(self,item_id):
 
 		if type(item_id, int) == False:
-			return { 'Error message':'Please supply an integer value'}
+			return { 'Error message': 'Please supply an integer value'}
 
 		item_list = []
 		for product in products_list:
@@ -48,3 +49,35 @@ class SpecificProduct(Resource):
 				return { 'product': item_list[0] }, 200
 
 		return { 'message': 'no such product found'}, 404
+
+class sale_orders(Resource):
+	def post(self):
+		data = request.get_json()
+
+		sale_id = len(sale_orders_list)  + 1
+		product_id = data['product_id']
+		products_tally = data['products_tally']
+		total_cost =data['total_cost']
+		attendant_id = data['attendant_id']
+
+		payload = {
+				'sale_id': sale_id,
+				'product_id': product_nid,
+				'products_tally' : products_tally,
+				'total_cost' : total_cost,
+				'attendant_id' : attendant_id
+				}
+
+		sale_item = [item for item in products_list if item['item_id'] == product_id]
+
+		if len(sale_item) > 0:
+			available = sale_item[0]['quantity']
+			if products_tally <= available:
+				rem = available - products_tally
+				sale_item[0].update('quantity' = rem)
+				return { 'success':'Order succesfuly placed'}
+		
+		return { 'error': 'product not in stock'}
+
+		
+
